38866604d8dd2ebe988172a3a2ad07914a8ec9c3
Theminecraft <paulschmid511+spam@gmail.com>
2024-07-18 18:40:34 +0200

    Hi

---
+++ quiz.js
@@ -0,0 +1,187 @@
+let question;
+let form;
+let res;
+let qno;
+let result;
+const questions = [
+        {
+            title: 'Welche Art von Filmen bevorzugst du?',
+            options: [
+                'Action',
+                'Komödie',
+                'Drama',
+                'Science-Fiction'
+            ],
+        },
+        {
+            title: 'Wie möchtest du die Handlung eines Films beschreiben?',
+            options: [
+                'Komplex und tiefgründig',
+                'Leicht und unterhaltsam',
+                'Herzzerreißend und emotional',
+                'Faszinierend und futuristisch'
+            ],
+        },
+        {
+            title: 'Welchen Schauplatz magst du am meisten?',
+            options: [
+                'Städtisch',
+                'Ländlich',
+                'Historisch',
+                'Außerweltlich'
+            ],
+        },
+        {
+            title: 'Welchen Hauptcharakter bevorzugst du?',
+            options: [
+                'Mutig und entschlossen',
+                'Witzig und charmant',
+                'Zerbrechlich und verletzlich',
+                'Intelligent und visionär'
+            ],
+        },
+        {
+            title: 'Wie sollte das Ende eines Films sein?',
+            options: [
+                'Überraschend und unerwartet',
+                'Glücklich und zufriedenstellend',
+                'Traurig und tragisch',
+                'Offen für Interpretationen'
+            ],
+        },
+        {
+            title: 'Welche visuellen Effekte magst du am meisten?',
+            options: [
+                'Spektakuläre Action-Szenen',
+                'Humorvolle visuelle Gags',
+                'Tiefe emotionale Momente',
+                'Faszinierende Weltgestaltung'
+            ],
+        },
+        {
+            title: 'Wie wichtig ist dir die Originalität der Handlung?',
+            options: [
+                'Sehr wichtig, ich mag einzigartige Geschichten',
+                'Ich mag bewährte und bekannte Geschichten',
+                'Ich bevorzuge klassische Erzählstrukturen',
+                'Ich mag innovative und kreative Ansätze'
+            ],
+        },
+        {
+            title: 'Welches Tempo bevorzugst du in einem Film?',
+            options: [
+                'Schnell und actiongeladen',
+                'Ruhig und entspannend',
+                'Langsam und bedächtig',
+                'Dynamisch und abwechslungsreich'
+            ],
+        },
+        {
+            title: 'Welche Art von Filmmusik magst du am meisten?',
+            options: [
+                'Eindrucksvoller Orchestersoundtrack',
+                'Lustige und eingängige Songs',
+                'Eindringliche und emotionale Melodien',
+                'Experimentelle und futuristische Klänge'
+            ],
+        },
+        {
+            title: 'Wie wichtig sind dir Nebencharaktere in einem Film?',
+            options: [
+                'Sie sollten eine bedeutende Rolle spielen',
+                'Sie sollten den Hauptcharakteren unterstützen',
+                'Sie sollten emotionale Tiefe verleihen',
+                'Sie sollten die Welt des Films bereichern'
+            ],
+        }
+    ];
+    
+
+
+function resetradio() {
+    const res = document.querySelector('#res');
+    document.querySelectorAll('[type="radio"]').forEach((radio) => {
+        radio.removeAttribute("disabled");
+    });
+    res.setAttribute("class","idle");
+    res.innerHTML = "Empty";
+}
+
+
+function getNextQuestion() {
+    if(qno == 9) {
+        const selectedOption = parseInt(form.op.value);
+        result = result * 10 + (selectedOption + 1);
+        
+        document.location.href = 'result.html?result=' + result;
+    }
+    else {
+        const selectedOption = parseInt(form.op.value);
+        result = result * 10 + (selectedOption + 1);
+        qno++;
+        ques = questions[qno];
+        question.innerHTML = ques.title;
+        console.log(result);
+        const labels = document.querySelectorAll('label');
+        labels.forEach((label, idx) => {
+            label.innerHTML = ques.options[idx];
+        }); 
+    }
+
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    if(!form.op.value) {
+        alert('Please select an option');
+    }
+    else if(form.submit.classList.contains('submit')) {
+        getNextQuestion();
+        form.submit.classList.remove('submit');
+        form.submit.value = "Submit"
+        form.submit.classList.add('submit');
+    }
+    
+    else if(form.submit.classList.contains('next')) {
+        console.log("test");
+        document.location.href = 'index.html'
+        form.submit.classList.remove('next');
+        form.submit.value = "Submit"
+        form.submit.classList.add('submit');
+        form.reset();
+    }
+}
+function init() {
+    document.body.innerHTML = `
+        <div class="app-body">
+            <div class="question-card">
+                <h2 id='question'>Question</h2>
+                <form>
+                    <input type="radio" id="op1" name="op" value="0">
+                    <label for="op1">op1</label><br>
+                    <input type="radio" id="op2" name="op" value="1">
+                    <label for="op2">op2</label><br>
+                    <input type="radio" id="op3" name="op" value="2">
+                    <label for="op3">op3</label><br>
+                    <input type="radio" id="op4" name="op" value="3">
+                    <label for="op4">op4</label><br>
+                    <input type="submit" name="submit" value = 'Submit' class = "submit"/>
+                </form>
+            </div>
+        </div>
+    `;
+   question = document.querySelector('#question');
+   form = document.querySelector('form');
+   res = document.querySelector('#res');
+   qno = 0;
+   result = 0;
+   form.addEventListener('submit', handleSubmit);
+   ques = questions[qno];
+   question.innerHTML = ques.title;
+   const labels = document.querySelectorAll('label');
+   labels.forEach((label, idx) => {
+    label.innerHTML = ques.options[idx];
+   }); 
+}
+init();
+
---
+++ result.js
@@ -0,0 +1,182 @@
+const questions = [
+    {
+        title: 'Welche Art von Filmen bevorzugst du?',
+        options: [
+            'Action',
+            'Komödie',
+            'Drama',
+            'Science-Fiction'
+        ],
+    },
+    {
+        title: 'Wie möchtest du die Handlung eines Films beschreiben?',
+        options: [
+            'Komplex und tiefgründig',
+            'Leicht und unterhaltsam',
+            'Herzzerreißend und emotional',
+            'Faszinierend und futuristisch'
+        ],
+    },
+    {
+        title: 'Welchen Schauplatz magst du am meisten?',
+        options: [
+            'Städtisch',
+            'Ländlich',
+            'Historisch',
+            'Außerweltlich'
+        ],
+    },
+    {
+        title: 'Welchen Hauptcharakter bevorzugst du?',
+        options: [
+            'Mutig und entschlossen',
+            'Witzig und charmant',
+            'Zerbrechlich und verletzlich',
+            'Intelligent und visionär'
+        ],
+    },
+    {
+        title: 'Wie sollte das Ende eines Films sein?',
+        options: [
+            'Überraschend und unerwartet',
+            'Glücklich und zufriedenstellend',
+            'Traurig und tragisch',
+            'Offen für Interpretationen'
+        ],
+    },
+    {
+        title: 'Welche visuellen Effekte magst du am meisten?',
+        options: [
+            'Spektakuläre Action-Szenen',
+            'Humorvolle visuelle Gags',
+            'Tiefe emotionale Momente',
+            'Faszinierende Weltgestaltung'
+        ],
+    },
+    {
+        title: 'Wie wichtig ist dir die Originalität der Handlung?',
+        options: [
+            'Sehr wichtig, ich mag einzigartige Geschichten',
+            'Ich mag bewährte und bekannte Geschichten',
+            'Ich bevorzuge klassische Erzählstrukturen',
+            'Ich mag innovative und kreative Ansätze'
+        ],
+    },
+    {
+        title: 'Welches Tempo bevorzugst du in einem Film?',
+        options: [
+            'Schnell und actiongeladen',
+            'Ruhig und entspannend',
+            'Langsam und bedächtig',
+            'Dynamisch und abwechslungsreich'
+        ],
+    },
+    {
+        title: 'Welche Art von Filmmusik magst du am meisten?',
+        options: [
+            'Eindrucksvoller Orchestersoundtrack',
+            'Lustige und eingängige Songs',
+            'Eindringliche und emotionale Melodien',
+            'Experimentelle und futuristische Klänge'
+        ],
+    },
+    {
+        title: 'Wie wichtig sind dir Nebencharaktere in einem Film?',
+        options: [
+            'Sie sollten eine bedeutende Rolle spielen',
+            'Sie sollten den Hauptcharakteren unterstützen',
+            'Sie sollten emotionale Tiefe verleihen',
+            'Sie sollten die Welt des Films bereichern'
+        ],
+    }
+];
+
+let answerall = ''; // Initialize as an empty string to accumulate answers
+
+const decodeUserChoices = (answers) => {
+    const decodedAnswers = [];
+
+    for (let i = 0; i < answers.length; i++) {
+        const optionIndex = parseInt(answers.charAt(i)) - 1;
+        const answerSentence = `Frage: ${questions[i].title.toLowerCase()} Antwort: ${questions[i].options[optionIndex].toLowerCase()}.`;
+        decodedAnswers.push(answerSentence);
+    }
+
+    return decodedAnswers;
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const userAnswers = urlParams.get('result');
+const decodedAnswers = decodeUserChoices(userAnswers);
+
+// Concatenate decoded answers into answerall string with newline separator
+decodedAnswers.forEach(answer => answerall += answer + '\n');
+
+// Log accumulated answers
+console.log(answerall);
+empfehlungengenrieren(answerall)
+async function empfehlungengenrieren(info) {
+    const loaderDiv = document.getElementById("loader");
+    loaderDiv.style.display = "block";
+    const apiKey = 'sk-pxNju75vKyXJVhjySvLCTdKxluNcijXvQDgFmpRRjZIWgAqe';
+    const endpoint = 'https://api.chatanywhere.tech/v1/chat/completions';
+
+    const requestData = {
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: 'You are an AI that will get information about a user and then you will select a movie for them. Only say the title, no sentence. Your response will be feed into an api so no sentence only the movie example : "Minions"'
+            },
+            {
+                role: 'user',
+                content: "Only say the title, no sentence. Your response will be feed into an api so no sentence only the movie" + info
+            }
+        ],
+        max_tokens: 50,
+        temperature: 0.7
+    };
+
+    try {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        const data = await response.json();
+        const movie = data.choices[0].message.content.trim();
+        console.log(movie);
+
+        try {
+            const resultDiv = document.getElementById("result");
+            const response = await fetch(`https://www.omdbapi.com/?t=${movie}&plot=full&apikey=d2c54f11`);
+            const data = await response.json();
+            const movieImageUrl = data.Poster || "";
+            const img = new Image();
+            img.src = movieImageUrl;
+            img.alt = data.Title;
+
+            img.onload = () => {
+                resultDiv.innerHTML += `
+                    <div class="alternative">
+                        <h4>${data.Title}</h4>
+                        <img src="${img.src}" alt="${img.alt}" />
+                        <p>Released: ${data.Released}</p>
+                    </div>
+                `;
+            };
+
+            img.onerror = () => { };
+        } catch (error) {
+            console.error(error);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+    loaderDiv.style.display = "none";
+}
